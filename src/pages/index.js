dff01261b72f1dd4b51bd49e426c1a8a82daf669
Willies Wanjala <gwiliez@gmail.com>
2023-01-30 18:53:52 +0300

    update the content

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,13 +13,12 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="Android Developer and Cloud DevOps Engineer" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
+        <InterestsSection sectionId="skills" heading="Skills" />
         <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
+        <ProjectsSection sectionId="projects" heading="Projects" />
         <ContactSection sectionId="github" heading="Issues?" />
       </Page>
     </>
